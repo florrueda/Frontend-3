dc80a5fbf22751f6fecdce8719551cda036eba04
Flor-Rueda <flor.rueda@gmail.com>
2023-02-24 01:25:20 -0300

    Mesa de trabajo

--- src/Components/PokemonList/Pokemons.jsx
+++ src/Components/PokemonList/Pokemons.jsx
@@ -5,9 +5,21 @@ import PokemonCard from "./PokemonCard";
 const Pokemons = ({type}) => {
 
     let pokemonsByType = pokemonsJson[type]
-    
+
+    function style () {
+        if(type === "water") {
+            return {backgroundColor: 'grey'}
+        } else if (type === "fire"){
+            return {backgroundColor: 'tomato'}
+        } else if(type === 'air') {
+            return {backgroundColor: 'green'}
+        } else {
+            return {backgroundColor: 'pink'}
+        }
+    } 
+
     return (
-        <div>
+        <div style={style()}>
             {pokemonsByType.map((pokemon) => {
                 return (
                 <PokemonCard pokemon={pokemon} key={pokemon.id} />
